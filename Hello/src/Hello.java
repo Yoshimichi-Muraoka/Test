d8a71c343e54420be721bec83a2521dd64992186
Yoshimichi Muraoka <bump03071117@gmail.com>
2021-04-29 10:11:33 +0900

    太郎を削除

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -2,7 +2,6 @@ public class Hello {
     
     public static void main(String[] args) {
         System.out.println("おはよう");
-        System.out.println("太郎");
         System.out.println("こんにちは");
     }
 }
