7e21c99a3acfb79d035bbce4b2a8e3a1fb9f591d
Yoshimichi Muraoka <bump03071117@gmail.com>
2021-04-30 08:08:26 +0900

    おはようを岡崎に変更

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     
     public static void main(String[] args) {
-        System.out.println("おはよう");
+        System.out.println("岡崎");
         System.out.println("本田");
         System.out.println("宇佐美");
         System.out.println("香川");
