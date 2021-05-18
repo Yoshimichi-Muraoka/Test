d118421110c421732d08e3486aeee97b67db09c3
Yoshimichi Muraoka <bump03071117@gmail.com>
2021-05-18 10:17:13 +0900

    NewFile追加

---
+++ NewFile/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    
+    public static int i =1;
+
+    public void method1 (){
+        System.out.println("method1");
+        System.out.println(i);
+    }
+
+    public void method2 (){
+        method1();
+        System.out.println("method2");
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        App app = new App();
+        app.method2();
+
+    }
+}
