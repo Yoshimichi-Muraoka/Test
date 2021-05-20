463e5f6482bfa3278cf391e8439d24e2f30fc4a8
Yoshimichi Muraoka <bump03071117@gmail.com>
2021-05-20 19:12:41 +0900

    コミットよろ

--- NewFile/src/App.java
+++ NewFile/src/App.java
@@ -22,5 +22,14 @@ public class App {
 
         }
 
+        int total =0;
+        a: for (int i=0; i<5; i++){
+            b: for(int j=0; j<5; j++){
+                if(i%2==0)continue a;
+                if(3<j)break b;
+                total +=j;
+            }
+        }
+        System.out.println(total);
     }
 }
