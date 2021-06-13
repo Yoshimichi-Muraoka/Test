d24d19533b3f0c4211bbf21ed618d98857314014
村岡恵道 <76888943+Yoshimichi-Muraoka@users.noreply.github.com>
2021-06-13 11:51:23 +0900

    波カッコの入れ子を変更
    波カッコの入れ子を変更

--- TestJDBC/src/controller/DbTest.java
+++ TestJDBC/src/controller/DbTest.java
@@ -26,29 +26,30 @@ public class DbTest extends HttpServlet {
 		Connection con =null;
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-
-			try {
-				con = DriverManager.getConnection(
-					"jdbc:mysql://localhost/mydb","root","bump0307"
-				);
-				request.getRequestDispatcher("/WEB-INF/test.jsp")
-				.forward(request, response);
-			} catch (SQLException e) {
-				System.out.println("コネクションオブジェクトを取得出来ません。");
-			} finally {
-				try {
-					if(con!=null) {
-						con.close();
-					}
-				} catch (SQLException e) {
-					System.out.println("コネクションを切断出来ませんでした。");
-				}
-			}
+			Class.forName("com.mysql.jdbc.Driver");]
 		} catch (ClassNotFoundException e) {
 			System.out.println("JDBCドライバをロード出来ません。");
 		}
 
+
+		try {
+			con = DriverManager.getConnection(
+				"jdbc:mysql://localhost/mydb","root","bump0307"
+			);
+			request.getRequestDispatcher("/WEB-INF/test.jsp")
+			.forward(request, response);
+		} catch (SQLException e) {
+			System.out.println("コネクションオブジェクトを取得出来ません。");
+		} finally {
+			try {
+				if(con!=null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("コネクションを切断出来ませんでした。");
+			}
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
